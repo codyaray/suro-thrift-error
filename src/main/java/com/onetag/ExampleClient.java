c95291c40c41eb0beb0c552db6fed63a03014630
Cody A. Ray <sites@codyaray.com>
2013-12-16 15:40:15 -0600

    Minimum example to reproduce suro thrift error

---
+++ src/main/java/com/onetag/ExampleClient.java
@@ -0,0 +1,26 @@
+package com.onetag;
+
+import com.netflix.suro.ClientConfig;
+import com.netflix.suro.client.SuroClient;
+import com.netflix.suro.message.Message;
+
+import java.util.Properties;
+
+public class ExampleClient {
+    public static void main(String[] args) {
+        // create the client
+        final Properties clientProperties = new Properties();
+        clientProperties.setProperty(ClientConfig.LB_TYPE, "static");
+        clientProperties.setProperty(ClientConfig.LB_SERVER, args[0]);
+        clientProperties.setProperty(ClientConfig.CLIENT_TYPE, "sync");
+
+        SuroClient client = new SuroClient(clientProperties);
+
+        // send the message
+        for (int i = 0; i < Integer.parseInt(args[1]); ++i) {
+            client.send(new Message("routingKey", "testMessage".getBytes()));
+        }
+
+        client.shutdown();
+    }
+}
